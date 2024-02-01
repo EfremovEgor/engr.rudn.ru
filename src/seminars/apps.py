ba45d6868e370545c992755f71f94794006fc410
Efremov Egor <efremovegor936@gmail.com>
2024-02-01 20:49:36 +0300

    change apps name

--- src/seminars/apps.py
+++ src/seminars/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class SeminarsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seminars'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "seminars"
+    verbose_name = "Семинары"

ba42ef7498349d0616b9d43c79fac44e2c5f7b3d
aa <aaa@MacBook-Air-aa.local>
2024-02-15 18:06:05 +0300

    push

--- src/pages/utils/aliases.py
+++ src/pages/utils/aliases.py
@@ -26,6 +26,7 @@ scientific_center_name_to_page = {
     "Международный научно-исследовательский центр реставрации и сохранения архитектурного наследия": "restoration_architectural_heritage.html",
     "Научный центр комплексного исследования и эффективной разработки недр": "subsoil_development.html",
     "Научный центр техники и технологий строительства": "technology_and_building.html",
+    "Научный центр транспортных систем и комплексов": "operational_systems_and_complexes.html"
     # "Учебный центр управления полетами"
 }
 # scientific_center_name_to_abbreviation =  {

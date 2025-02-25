cf18ce3143e8d4c4354eaaa9fe5ce56741ab231b
Mikhail <lebedev_ma@pfur.ru>
2025-02-25 13:01:27 +0300

    models for news

--- src/news/models.py
+++ src/news/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.utils import timezone
+from PIL import Image
+import io
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 class Tag(models.Model):
@@ -28,7 +31,7 @@ class NewsItem(models.Model):
         blank=False,
         null=False,
     )
-    creation_date = models.DateTimeField(verbose_name="Название", default=timezone.now)
+    creation_date = models.DateTimeField(verbose_name="Дата создания", default=timezone.now)
 
     class Meta:
         verbose_name = "Новость"
@@ -36,3 +39,20 @@ class NewsItem(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def save(self, *args, **kwargs):
+        if self.preview_image:
+            img = Image.open(self.preview_image)
+            img = img.convert('RGB')
+
+            img = img.resize((600, 450))
+
+            thumb_io = io.BytesIO()
+            img.save(thumb_io, 'JPEG', quality=85)
+            thumb_io.seek(0)
+
+            self.preview_image = InMemoryUploadedFile(
+                thumb_io, None, self.preview_image.name, 'image/jpeg', thumb_io.getbuffer().nbytes, None
+            )
+        
+        super().save(*args, **kwargs)
